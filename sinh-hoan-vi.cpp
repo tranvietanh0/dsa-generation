1b9b489243487a8f5ffec226ed3add013279194e
tranvietanh0 <tranvietanh0507@gmail.com>
2024-02-17 17:05:06 +0700

    sinh hoan vi basic

---
+++ sinh-hoan-vi.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int n, a[1000], final = 0;
+void ktao(){
+    cin >> n;
+    for (int i = 1; i <= n; i++){
+        a[i] = i;
+    }
+}
+void sinh(){
+    int i = n - 1;
+    while(i >= 1 && a[i] > a[i + 1]){
+        --i;
+    }
+    if(i == 0){
+        final = 1;
+    }
+    else{
+        int j = n;
+        while(a[i] > a[j]){
+            --j;
+        }
+        swap(a[i], a[j]);
+        sort(a + i + 1, a + n + 1);
+    }
+}
+int main(){
+    ktao();
+    while(final == 0){
+        for (int i = 1; i <= n; i++){
+            cout << a[i] << " ";
+        }
+        cout << endl;
+        sinh();
+    }
+    return 0;
+}
